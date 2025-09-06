43f1900b8279e8decce6455ead22304152f7b84e
Ailun Shi <ailunshi@berkeley.edu>
2025-09-05 18:07:23 -0700

    Fix UI of home page, again.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="grid sm:grid-cols-2 min-h-screen items-center h-full w-full sm:p-10">
-            <div className="grid grid-rows-[auto]">
+        <div className="flex flex-col md:grid md:grid-cols-2 min-h-screen justify-center items-center h-full w-full sm:p-10">
+            <div className="text-center md:grid md:grid-rows-[auto]">
                 <h1 className="alata text-moon-yellow text-[48px] md:text-[72px] lg:text-[96px]">AILUN SHI</h1>
                 <h1 className="zhimangxing text-[72px] md:text-[96px] lg:text-[120px]">施蔼伦</h1>
+                <h2 className="raleway italic text-moon-yellow">YA/Adult Fantasy Novelist</h2>
             </div>
 
-            <div className="-mt-130 sm:mt-0 text-left sm:text-right">
+            <div className="pt-20 md:pt-0 md:text-right">
                 <Link href="/about" className="cherish text-[72px] md:text-[120px]">enter &rarr;</Link>
             </div>
             
